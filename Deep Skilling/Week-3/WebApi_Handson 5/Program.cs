104be0c83c633aa70f77172362f981248b4c6456
akshaya-1192006 <akshayareddys.dondeti@gmail.com>
2026-07-20 19:06:46 +0530

    Implement JWT authentication in Program.cs
    Set up JWT authentication and authorization for the Web API.

---
+++ Deep Skilling/Week-3/WebApi_Handson 5/Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = "mySystem",
+        ValidAudience = "myUsers",
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes("mysuperdupersecret"))
+    };
+});
+
+builder.Services.AddAuthorization();
+var app = builder.Build();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
