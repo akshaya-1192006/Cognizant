c8e875dbfd5b9f4997acd8114a6adbf49ff4ca64
akshaya-1192006 <akshayareddys.dondeti@gmail.com>
2026-07-20 18:58:39 +0530

    Delete Deep Skilling/Week-3/WebApi_Handson 4/Models directory

--- Deep Skilling/Week-3/WebApi_Handson 4/Models/Employee.cs
+++
@@ -1,9 +0,0 @@
-namespace WebApi_Handson_4.Models
-{
-    public class Employee
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public double Salary { get; set; }
-    }
-}
