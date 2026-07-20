73eb08986de3c90a6c980f6e339c490bf7a7cc68
akshaya-1192006 <akshayareddys.dondeti@gmail.com>
2026-07-20 22:36:50 +0530

    Add initial Program.cs for Retail Inventory System

---
+++ Deep Skilling/Week-2/Entity Framework Core/Lab 1 - RetailInventory/Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace RetailInventory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entity Framework Core Demo");
+            Console.WriteLine("Retail Inventory System");
+            Console.WriteLine("ORM maps C# classes to SQL tables.");
+        }
+    }
+}
