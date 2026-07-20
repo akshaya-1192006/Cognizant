b2ff67d50020a2ab0aaa56f651390b4958ba8055
akshaya-1192006 <akshayareddys.dondeti@gmail.com>
2026-07-20 19:11:16 +0530

    Add ChatConsumer for Kafka message consumption

---
+++ Deep Skilling/Week-3/WebApi_Handson 6/ChatConsumer.cs
@@ -0,0 +1,15 @@
+using Confluent.Kafka;
+var config = new ConsumerConfig
+{
+    BootstrapServers = "localhost:9092",
+    GroupId = "group1",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+using var consumer = new ConsumerBuilder<Ignore, string>(config).Build();
+consumer.Subscribe("chat-message");
+Console.WriteLine("Waiting for messages...");
+while (true)
+{
+    var result = consumer.Consume();
+    Console.WriteLine("Received : " + result.Message.Value);
+}
