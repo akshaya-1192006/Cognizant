ad965012d04525d71e6dc087b84e2abbc344f051
akshaya-1192006 <akshayareddys.dondeti@gmail.com>
2026-07-21 15:47:54 +0530

    Add App component for Score Calculator App

---
+++ Deep Skilling/Week-5/React/Lab 3: Score Calculator App/App.js
@@ -0,0 +1,16 @@
+import CalculateScore from "./Components/CalculateScore";
+
+function App() {
+  return (
+    <div>
+      <CalculateScore
+        Name="Akshaya"
+        School="Vignan University"
+        Total={480}
+        Goal={6}
+      />
+    </div>
+  );
+}
+
+export default App;
