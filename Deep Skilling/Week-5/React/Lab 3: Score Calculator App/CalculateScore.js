2c61c2b5ef6fb01cba4eb0e8cc84679b15f75fd2
akshaya-1192006 <akshayareddys.dondeti@gmail.com>
2026-07-21 15:46:55 +0530

    Add CalculateScore component for score calculation

---
+++ Deep Skilling/Week-5/React/Lab 3: Score Calculator App/CalculateScore.js
@@ -0,0 +1,18 @@
+import "../Stylesheets/mystyle.css";
+
+function CalculateScore(props) {
+    const average = props.Total / props.Goal;
+    return (
+        <div className="box">
+            <h2>Student Details</h2>
+
+            <p><b>Name:</b> {props.Name}</p>
+            <p><b>School:</b> {props.School}</p>
+            <p><b>Total Marks:</b> {props.Total}</p>
+            <p><b>Subjects:</b> {props.Goal}</p>
+            <p><b>Average:</b> {average}</p>
+        </div>
+    );
+}
+
+export default CalculateScore;
